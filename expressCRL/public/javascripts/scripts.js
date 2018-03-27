a0338d4c5add09c6cf7dfda6eeb2faabdac5d5ea
Claudia Rangel <crl2316@gmail.com>
2018-03-26 20:21:01 -0500

    Added folders express, resitify both working with sequelizer and mysql to initiate group project

---
+++ expressCRL/public/javascripts/scripts.js
@@ -0,0 +1,226 @@
+$('document').ready(function() {
+    renderExistingSwimlanes();
+   
+    $('button').on('click', function() {
+        var swimlaneName = prompt('New swimlane name');
+            if(swimlaneName == null){
+                return null;
+            } 
+           
+
+        var id = getNewId();
+        drawSwimlane(id, swimlaneName);
+        saveSwimlane({id: id, name: swimlaneName});   
+    });
+});
+
+var newSwimlane;
+
+function renderExistingSwimlanes() {
+    $.ajax({
+            method: "GET",
+            url: "http://localhost:8080/swimlanes",
+
+        })
+        .done(function(swimlanes) {
+            console.log(swimlanes);
+
+            for (var i = 0; i < swimlanes.length; i++) {
+                var swimlane = swimlanes[i];
+                drawSwimlane(swimlane.id, swimlane.name);
+                
+                // Get cards for swimlane by swimlaneID
+                renderExistingCards(swimlane.id);
+            }
+        });
+}
+
+function renderExistingCards(swimlaneId) {
+    $.ajax({
+            method: "GET",
+            url: 'http://localhost:8080/swimlanes/' + swimlaneId + '/cards',
+
+        })
+        .done(function(cards) {
+            console.log(cards);
+
+            for (var i = 0; i < cards.length; i++) {
+                drawCard(cards[i].id, swimlaneId, cards[i].name, cards[i].cardDescription);
+            }
+        });
+}
+
+function getNewId(){
+    var date = new Date();
+    var id = date.getTime();
+
+    console.log(id);
+
+    return id;
+}
+
+function drawSwimlane(id, name) {
+	newSwimlane = $('<div id="' + id +'" class="swimlane"></div>');
+
+    newSwimlane.draggable({
+        start: function() {
+            $(this).css("zIndex", 100);
+        }
+    });
+    newSwimlane.droppable({
+        drop: function(event, ui) {
+            var otherSwimlane = ui.draggable;
+            var thisSwimlane = $(this);
+
+            otherSwimlane.detach();
+            otherSwimlane.insertAfter(thisSwimlane);
+            otherSwimlane.css("zIndex", 0);
+
+        }
+    });
+
+    var swimlaneHeader = $('<div class="swimlaneHeader">' + name + '</div>');
+    newSwimlane.append(swimlaneHeader);
+
+    var buttons = $('<div class="buttons"><i class="fas fa-trash-alt icons"></i><i class="fas fa-pencil-alt icons"></i><i class="fas fa-plus icons"></i></div>');
+    newSwimlane.append(buttons);
+
+    buttons.on('click', '.fa-trash-alt', function() {
+        $(this).closest('.swimlane').remove();
+
+    });    
+
+    buttons.on('click', '.fa-pencil-alt', function() {
+        var newName = prompt('New swimlane name');
+         swimlaneHeader.text(newName);
+
+        //update header with new name
+        // save new name to restify server
+
+        updateSwimlane(id, newName);
+        
+      
+    });
+
+    buttons.on('click', '.fa-plus', function() {
+        var cardHeader = prompt('New card name');
+            if(cardHeader == null){
+                return null;
+            } 
+        var cardDescription = prompt('Card description required');
+            if(cardDescription == null){
+                return null;
+            } 
+        
+        var cardId = getNewId();
+        drawCard(cardId, id, cardHeader, cardDescription);       
+        saveCard({id: cardId, swimlane_id: id, name: cardHeader, cardDescription: cardDescription});
+    })
+
+    $('#swimlanes').append(newSwimlane);
+}
+
+function drawCard(cardId, swimlaneId, name, cardDescription) {
+	
+    	var card = $('<div class="card"></div>');
+
+        card.draggable({
+             start: function() {
+            $(this).css("zIndex", 100);
+            }
+        });
+        card.droppable({
+            drop: function(event, ui) {
+                var otherCard = ui.draggable;
+                var thisCard = $(this);
+
+                otherCard.detach();
+                otherCard.insertBefore(thisCard);
+                otherCard.css("zIndex", 0);
+            }
+        });
+
+        var cardHeader = $('<div class="cardHeader">' + name + '</div>');
+        card.append(cardHeader);
+
+        var cardButtons = card.append('<div class="buttons"><i class="fas fa-trash-alt icons"></i><i class="fas fa-pencil-alt pencil_card icons"></i></div>');
+        
+        var cardDescription = $('<div class="cardDescription">' + cardDescription +'</div>');
+        card.append(cardDescription);
+
+        $("#"+ swimlaneId).append(card);
+
+        cardButtons.on('click', '.fa-trash-alt', function() {
+            $(this).closest('.card').remove();
+        });
+
+        cardButtons.on('click', '.pencil_card', function() {
+             var newCardName = prompt('New card name');
+            cardHeader.text(newCardName);
+
+              updateCard(cardId, newCardName);
+        });
+
+        cardDescription.on('click', function(){
+            var newCardDescription = prompt("New card description");
+            cardDescription.text(newCardDescription);
+
+            updateCardDescription(cardId, newCardDescription);
+
+        })
+    }
+
+function saveSwimlane(swimlane) {
+    $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/swimlanes",
+            data: swimlane
+        })
+        .done(function(swimlane) {
+            alert("Swimlane Saved: " + swimlane);
+        });
+}
+
+function updateSwimlane(id, name){
+    $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/swimlanes/" + id,
+            data: {name: name}
+        })
+        .done(function(swimlane) {
+            alert("Swimlane Updated: " + swimlane);
+        });
+}
+
+function updateCard(id, name){
+    $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/swimlanes/cards/" + id,
+            data: {name: name}
+        })
+        .done(function(card) {
+            alert("Card Updated: " + card);
+        });
+}
+
+function updateCardDescription(id, cardDescription){
+    $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/swimlanes/cards/" + id,
+            data: {cardDescription: cardDescription}
+        })
+        .done(function(card) {
+            alert("Card description Updated: " + card);
+        });
+}
+
+function saveCard(card) {
+    $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/cards",
+            data: card
+        })
+        .done(function(card) {
+            alert("Card Saved: " + card);
+        });
+}
---
+++ restifyCRL/app.js
@@ -0,0 +1,187 @@
+// Require the modules
+var restify = require('restify');
+var server = restify.createServer();
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+// Set up restify
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+// setup the mysql configuration
+const sql = new Sequelize('swimlanes', 'root', 'Luna12094*', {
+	host: 'localhost',
+	port: 3306,
+	dialect: 'mysql',
+	operatorsAliases: false,
+	pool: {
+		max: 5,
+		min: 0,
+		acuire: 30000,
+		idle: 10000
+	}
+});
+
+// make the connection
+sql
+	.authenticate()
+	.then(() => {
+		console.log("The connection was successful!");
+	})
+	.catch(err => {
+		console.log("There was an error when connecting!");
+	});
+
+var Swimlanes = sql.define('swimlane', {
+	id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV4 },
+	name: { type: Sequelize.STRING }
+});
+
+var Cards = sql.define('card', {
+	id: {type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV4},
+	name: {type: Sequelize.STRING},	
+	cardDescription: {type: Sequelize.STRING}
+});
+
+
+// create associations
+Cards.Swimlanes = Cards.belongsTo(Swimlanes);
+
+sql.sync();
+
+function getSwimlanes(req, res, next) {
+	// Restify currently has a bug which doesn't allow you to set default headers
+	// These headers comply with CORS and allow us to serve our response to any origin
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+	//find the appropriate data
+	Swimlanes.findAll().then((swimlanes) => {
+		res.send(swimlanes);
+	});	
+}
+
+function getCards(req, res, next) {
+	// Restify currently has a bug which doesn't allow you to set default headers
+	// These headers comply with CORS and allow us to serve our response to any origin
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+	//find the appropriate data
+	Cards.findAll().then((cards) =>{
+		res.send(cards);
+	});
+}
+
+function postSwimlane(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	
+	console.log(req.body);
+
+	
+	// save the new message to the collection
+	Swimlanes.create({
+		id: req.body.id,
+		name: req.body.name
+	}).then((swimlane) => {
+		res.send(swimlane);
+	});	
+}
+
+function postCard(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	
+	console.log(req.body);
+
+	
+	// save the new message to the collection
+	Swimlanes.findAll({
+		where: {id: req.body.swimlane_id}
+	})
+	.then((swimlanes) => {
+		Cards.create({
+			id: req.body.id,
+			name: req.body.name,
+			cardDescription: req.body.cardDescription
+		}).then((card) => {
+			card.setSwimlanes(swimlanes[0]);
+			res.send(card);
+		});
+	});	
+}
+
+function getCardsBySwimlaneId (req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	
+	console.log(req.params)
+
+	Cards.findAll({
+		where: {swimlaneId: req.params.swimlane_id}
+	})
+	.then((cards) => {
+		res.send(cards);
+	});
+}
+
+function updateSwimlaneById (req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	
+	// get swimlaneId from URL
+	var swimlaneId =  req.params.swimlane_id;
+
+	// get newSwimlane from body
+	var name = req.body.name;
+
+	// find swimlane by swimlane ID
+	// var swimlane = swimlanes.find(function(swimlane){
+	// 	return swimlane.id == swimlaneId;
+	// });
+
+	// update swimlanes name property
+	// swimlane.name = name;
+
+	// return swimlane to caller
+	// res.send(swimlane);
+}
+
+function updateCardById (req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	
+	var card_id = req.params.card_id;
+
+	// var card = cards.find(function(card){
+	// 	return card.id == card_id;
+	// });
+	// if(req.body.name){
+		// card.name = req.body.name;
+	// }
+	// if(req.body.description){
+		// card.cardDescription = req.body.description;
+	//}
+	// res.send(card);
+}
+
+
+
+// Set up our routes and start the server
+server.get('/swimlanes', getSwimlanes);
+server.post('/swimlanes', postSwimlane);
+
+server.get('/swimlanes/:swimlane_id/cards', getCardsBySwimlaneId);
+
+server.post('/swimlanes/:swimlane_id', updateSwimlaneById);
+
+server.post('/swimlanes/cards/:card_id', updateCardById);
+
+server.get('/cards', getCards);
+server.post('/cards', postCard);
+
+server.listen(8080, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
